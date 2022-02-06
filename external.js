fe20dc6c69f371746e5db13c02d37d092749b45d
Ryan Buech <ryanbuech@gmail.com>
2022-02-06 04:06:24 -0600

    Photography update

--- external.js
+++ external.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -10,7 +12,7 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
+	if (scroll_position > 50) {
 		header.style.backgroundColor = '#303030';
 	} else {
 		header.style.backgroundColor = 'transparent';
@@ -27,3 +29,9 @@ menu_item.forEach((item) => {
 function removeHash () { 
     history.pushState("", document.title, window.location.pathname + window.location.search);
 };
+
+function openFullImgg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+};
+
